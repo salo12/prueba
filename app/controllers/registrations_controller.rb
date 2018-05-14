9ab3bdfc61a0bf4e1b211dd046a77739d8018598
salo12 <31453092+salo12@users.noreply.github.com>
2018-05-14 17:02:56 -0400

    registration

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,39 +1,17 @@
 class RegistrationsController < Devise::RegistrationsController
     respond_to :json
     # POST /resource
-   def create
-    build_resource(sign_up_params)
+    def create
 
-    resource.save
-    yield resource if block_given?
-    if resource.persisted?
-      if resource.active_for_authentication?
-        set_flash_message :notice, :signed_up if is_flashing_format?
-        #find_message is devise method. We used for json response
-        msg = find_message(:signed_up, {})
-        sign_up(resource_name, resource)
-        respond_with(resource) do |format|
-          format.json { render json: {url: after_sign_up_path_for(resource)}, status: 200 }
-        end
-      else
-        set_flash_message :notice, :"signed_up_but_#{resource.inactive_message}" if is_flashing_format?
-        #find_message is devise method. We used for json response
-        msg = find_message(:"signed_up_but_#{resource.inactive_message}", {})
-        expire_data_after_sign_in!
-        respond_with(resource) do |format|
-          format.json { render json: {message: msg,url: after_inactive_sign_up_path_for(resource)}, status: 200 }
-        end
-      end
-    else
-      clean_up_passwords resource
-      #passing block to handle error in signup for json
-      #http://edgeapi.rubyonrails.org/classes/ActionController/Responder.html
-      respond_with(resource) do |format|
-        msg = resource.errors.full_messages.join("<br>").html_safe
-        format.json { render json: {message: msg}, status: 401 }
-      end
-    end
+     user = User.new(params[:user])
+     if user.save
+       render :json=> user.as_json(:auth_token=>user.authentication_token, :email=>user.email), :status=>201
+       return
+     else
+       warden.custom_failure!
+       render :json=> user.errors, :status=>422
+     end
    end
-   
+
 
 end
