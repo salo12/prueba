9d721cc04b9449a0bbec4cfad915e6fa942a44d5
salo12 <31453092+salo12@users.noreply.github.com>
2018-05-23 17:20:10 -0400

    data

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
-  resources :autors
+
 root :to => "welcome#index"
 
   #root  "libros#index"
   devise_for :users
   resources :libros
+  resources :autors
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
