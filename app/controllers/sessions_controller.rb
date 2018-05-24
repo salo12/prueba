3bebcca3cab70f3ddb6764308f7f093f55f4f13b
salo12 <31453092+salo12@users.noreply.github.com>
2018-05-23 21:49:54 -0400

    usuario

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,5 @@
+class SessionsController < Devise::SessionsController
+
+    # POST /resource/sign_in
+
+end
--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,2 +1,6 @@
 class UserController < ApplicationController
+  before_action :authenticate_user!
+  def index
+      @users = User.all
+  end
 end
